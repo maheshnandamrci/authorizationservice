2f59a02f4470ae992976400cfbae4a217116cfdf
maheshnandamrci <Madhu@2207>
2021-10-04 15:39:06 -0400

    updated the deleted the unwanted kafka consumer code from producer

--- src/main/java/com/amdocs/media/authorizationservice/config/UserConstants.java
+++ src/main/java/com/amdocs/media/authorizationservice/config/UserConstants.java
@@ -12,7 +12,7 @@ public class UserConstants {
 	public static final String USER_AUTHORIZED = "User is authorized";
 	public static final String USER_NOT_AUTHORIZED = "User is not authorized";
 	public static final String USER_NOT_IN_DB = "User not found in DB";
-	public static String USER_PROFILE_ENDPOINT_URL = "http://localhost:9839/assignement/profile";
+	public static String USER_PROFILE_ENDPOINT_URL = "http://localhost:9080/assignement/profile";
 
 	public static final String CREATE = "Create";
 	public static final String UPDATE = "Update";
